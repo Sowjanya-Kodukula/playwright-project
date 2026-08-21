f077ab771958cdbf5aafc245edb8b42aa39bd45a
Sowjanya-Kodukula <sowjanyalalitha@gmail.com>
2026-08-21 19:48:43 +0530

    add: remove added items from home page tests

--- src/test/java/com/example/tests/AddItemsToCart.java
+++ src/test/java/com/example/tests/AddItemsToCart.java
@@ -1,14 +1,49 @@
-package com.example.tests;
+package test.java.com.example.tests;
 
 import com.example.utils.actions.AddItemsToCartAction;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AddItemsToCart {
     @Test
-    public void addItemsToCart() {
+    public void addItemsToCartandVerifyShoppingBadge() {
         try(AddItemsToCartAction a = new AddItemsToCartAction()){
+
             a.addItemsToCartAction();
+
         }
+    }
+
+    @Test
+    public void addItemsToCartAndRemoveItemsInHomePage() {
+
+        try (AddItemsToCartAction a = new AddItemsToCartAction()) {
+
+            a.addItemsToCartAction();
+
+            a.removeSelectedItemsFromHomePage();
+
+            Assertions.assertFalse(a
+                    .getPage()
+                    .locator(".shopping_cart_badge")
+                    .isVisible());
+
+        }
+
+    }
+
+    @Test
+
+    public void addItemsToCartAndVerifyItemsInShoppingCartPage() {
+
+        try(AddItemsToCartAction a = new AddItemsToCartAction()){
+
+            a.addItemsToCartAction();
+
+            a.goToShoppingCartAndVerifyItems();
+
+        }
+
     }
 }
